50b392bd9dda81c2775ada8fd332b05947c02784
Adınız <siz@e-posta.com>
2021-06-18 14:18:50 +0300

    fix cpuctl

--- src/cpucli.py
+++ src/cpucli.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 import cpu
-print(sys.argv[2],sys.argv[3]=="1"
 if sys.argv[1] == "governor":
     for i in range(0,cpu.cpucount()):
         cpu.change_cpu_governor(i,sys.argv[2])

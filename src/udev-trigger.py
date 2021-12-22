867c056149928bb81ffd457c5f63b4f04525e7d4
aliriza <aliriza.keskin@pardus.org.tr>
2021-12-22 17:00:26 +0300

    backlight level changed AC plug & unplug

--- src/udev-trigger.py
+++ src/udev-trigger.py
@@ -2,9 +2,12 @@
 import sys
 import os
 import tools.profile
+import tools.backlight
 import tools.utils
 import config
 config = config.config()
+
+
 if not tools.utils.checkIfProcessRunning("pardus-power-manager"):
     sys.exit(0)
 if config.get("udev-enabled","True").lower() != "true":
@@ -15,7 +18,13 @@ if os.environ["POWER_SUPPLY_ONLINE"] == "1":
 else:
     profile = config.get("ppm-mode-battery","1")
     tools.profile.set_profile(int(profile))
-    
+
+brightness_array = [10, 30, 55, 75, 100]
+for device in tools.backlight.get_devices():
+    percent = tools.backlight.get_max_brightness(device)/100
+    brightness_value = brightness_array[int(profile)]*percent
+    tools.backlight.set_brightness(device,brightness_value)
+
 if os.path.exists("/run/ppm"):
     f = open("/run/ppm","w")
     f.write(os.environ["POWER_SUPPLY_ONLINE"])

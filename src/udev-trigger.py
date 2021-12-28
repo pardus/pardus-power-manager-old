299a684df5a552083ad03297bd5e9472e7bac8ae
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2021-12-28 07:04:01 +0000

    udev trigger last status

--- src/udev-trigger.py
+++ src/udev-trigger.py
@@ -10,8 +10,20 @@ config = config.config()
 
 if not tools.utils.checkIfProcessRunning("pardus-power-manager"):
     sys.exit(0)
+
 if config.get("udev-enabled","True").lower() != "true":
     exit(0)
+
+ac_online = tools.profile.get_ac_online()
+
+if os.path.exists("/run/ppm.last"):
+    if str(ac_online) in tools.utils.readfile("/run/ppm.last"):
+        sys.exit(0)
+
+status=open("/run/ppm.last","w")
+status.write(str(ac_online))
+status.close()
+
 if tools.profile.get_ac_online():
     profile = config.get("ppm-mode-ac","3")
     tools.profile.set_profile(int(profile))
@@ -19,11 +31,12 @@ else:
     profile = config.get("ppm-mode-battery","1")
     tools.profile.set_profile(int(profile))
 
-brightness_array = [10, 30, 55, 75, 100]
-for device in tools.backlight.get_devices():
-    percent = tools.backlight.get_max_brightness(device)/100
-    brightness_value = brightness_array[int(profile)]*percent
-    tools.backlight.set_brightness(device,brightness_value)
+if config.get("udev-brightness","True").lower() != "true":
+    brightness_array = [10, 30, 55, 75, 100]
+    for device in tools.backlight.get_devices():
+        percent = tools.backlight.get_max_brightness(device)/100
+        brightness_value = brightness_array[int(profile)]*percent
+        tools.backlight.set_brightness(device,brightness_value)
 
 if os.path.exists("/run/ppm"):
     f = open("/run/ppm","w")

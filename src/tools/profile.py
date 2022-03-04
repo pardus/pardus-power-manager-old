fe17730f947afa8dd73bc0766e926393588ba714
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2022-03-04 12:26:54 +0000

    battery status logger and change alogrithm

--- src/tools/profile.py
+++ src/tools/profile.py
@@ -2,6 +2,8 @@ import os
 import dbus
 
 from tools.utils import asynchronous, readfile
+import datetime
+date = datetime.datetime.now()
 
 def is_support_charge_limit():
     """Charge level limit support"""
@@ -51,8 +53,23 @@ def get_ac_online():
     for device in os.listdir("/sys/class/power_supply/"):
         if os.path.exists("/sys/class/power_supply/{}/status".format(device)):
             status = readfile("/sys/class/power_supply/{}/status".format(device)).lower()
-            if "discharging" in status or "not charging" in status or "full" in status:
+            log.write("EVENT=\"battery-status\"\tBATTERY_DEVICE=\"{0}\"\tDATE=\"{1}\"\tSTATUS=\"{2}\"\n".format(
+                 device,
+                 date,
+                 status)
+            )
+            if "discharging" in status:
                 return False
+            elif "not charging" in status:
+                return True
+            elif "charging" in status:
+                return True
+            elif "full" in status:
+                return True
+            elif "empty" in status:
+                return False
+            elif "unknown" in status:
+                return True
     return True
 
 @asynchronous

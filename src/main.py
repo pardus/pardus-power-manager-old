f62f01d192c5af330886c8e6d7917f9a3a1ee132
aliriza <aliriza.keskin@pardus.org.tr>
2022-04-18 08:41:30 +0000

    force-enable question added for non-laptop

--- src/main.py
+++ src/main.py
@@ -60,29 +60,37 @@ if __name__ == "__main__":
     config = config.config()
     if tools.detect.is_virtual_machine():
         print("Virtual machine detected!")
-    elif not tools.detect.is_laptop():
-        tools.profile.set_service_status(False) # Disable service
-        if "--autostart" not in sys.argv:
-            # force enable application for some buggy laptops or testing
-            if config.get("force-enable-app","false").lower() != "true":
-                error_message=_("Your computer does not need power management.")
-                dialog = Gtk.MessageDialog(
-                    transient_for=None,
-                    flags=0,
-                    message_type=Gtk.MessageType.INFO,
-                    buttons=Gtk.ButtonsType.OK,
-                    text=error_message,
-                )
-                print(error_message,file=sys.stderr)
-                dialog.run()
-                dialog.destroy()
-            sys.exit(0) # exit application
     if tools.detect.is_live():
         tools.profile.set_profile(4) # extreme performance mode
     if tools.detect.is_docker() or tools.detect.is_chroot():
         sys.exit(0) # exit if docker or chroot
     if not tools.detect.is_root():
         sys.exit(0) # exit if non-root user.
+    if not tools.detect.is_laptop():
+        tools.profile.set_service_status(False) # Disable service
+        if "--autostart" not in sys.argv:
+            # force enable application for some buggy laptops or testing
+            if config.get("force-enable-app","false").lower() != "true":
+                error_message=_("Your computer does not need power management.")+ "\n"
+                error_message+=_("Do you want to enable it anyway?")
+                dialog = Gtk.MessageDialog(
+                    transient_for=None,
+                    flags=0,
+                    message_type=Gtk.MessageType.QUESTION,
+                    buttons=Gtk.ButtonsType.NONE,
+                    text=error_message,
+                )
+                dialog.add_button(_("No"), Gtk.ResponseType.NO)
+                dialog.add_button(_("Yes"), Gtk.ResponseType.YES)
+                response = dialog.run()
+                dialog.destroy()
+                if response in (Gtk.ResponseType.YES, Gtk.ResponseType.APPLY,
+                    Gtk.ResponseType.OK, Gtk.ResponseType.ACCEPT):
+                    config.set("force-enable-app","true")
+                    tools.profile.set_service_status(True)
+                else:
+                    sys.exit(0) # exit application
+
 
 
     if config.get("is-app-active","true").lower() != "true":

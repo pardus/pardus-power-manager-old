42cffd75c0b70a5639a3e8e251ca53a16617eabd
Kerem Göksu <superkerem13@gmail.com>
2023-02-18 16:37:17 +0300

    Reformat to be more readable, faster and idiomatic

--- src/tools/config.py
+++ src/tools/config.py
@@ -1,37 +1,36 @@
 __VARS={}
 
 def read_config():
-    config = open("/etc/pardus-power-manager.conf","r")
-    for line in config.read().split("\n"):
-        if "#" in line:
-            line = line.split("#")[0]
-        if "=" in line:
-            variable = line.split("=")[0]
-            value = line.split("=")[1]
-            __VARS[variable] = value
-    config.close()
+    with open("/etc/pardus-power-manager.conf", "r") as config:
+        for line in config.read().split("\n"):
+            if "#" in line:
+                line = line.split("#")[0]
+            if "=" in line:
+                variable = line.split("=")[0]
+                value = line.split("=")[1]
+                __VARS[variable] = value
 
 def get_value(variable):
-    if variable in __VARS:
-        value = __VARS[variable]
-        if value in ["true","false"]:
-            return value == "true"
-        if value.isnumeric():
-            return int(value)
-        return value
-    return ""
+    value = __VARS.get(variable)
+    if value is None:
+        return ""
+    if value in ["true", "false"]:
+        return value == "true"
+    if value.isnumeric():
+        return int(value)
+    return value
 
-def set_value(variable,value):
-    if variable in __VARS:
-        if value == True:
-            value = "true"
-        if value == False:
-            value = "false"
-        __VARS[variable] = str(value)
+def set_value(variable, value):
+    if __VARS.get(variable) is None:
+        return
+    if value == True:
+        value = "true"
+    if value == False:
+        value = "false"
+    __VARS[variable] = str(value)
 
 def write_config():
-    config = open("/etc/pardus-power-manager.conf","w")
-    config.write("[main]\n")
-    for variable in __VARS.keys():
-        config.write("{}={}\n".format(variable,__VARS[variable]))
-    config.close()
+    with open("/etc/pardus-power-manager.conf", "w") as config:
+        config.write("[main]\n")
+        for variable, value in __VARS.values():
+            config.write("{}={}\n".format(variable, value))
